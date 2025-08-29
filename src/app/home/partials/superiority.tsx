9046c76cfd846f9e469db134f383e8ff24a8fda4
adi <adisudana@gmail.com>
2025-08-29 19:09:25 +0700

    fix partial pages

--- src/app/home/partials/superiority.tsx
+++ src/app/home/partials/superiority.tsx
@@ -1,13 +1,65 @@
+import { Star } from 'lucide-react';
+import Image from 'next/image';
 import React from 'react';
 
+import AnimatedHeading from '@/components/ui/animated-heading';
 import { Button } from '@/components/ui/button';
+
+import { withMe, withOther } from '@/constants/superiority-data';
 
 const Superiority = () => {
   return (
-    <section className='custom-container mb-20'>
+    <section className='custom-container mt-20 mb-20'>
       <Button variant='outline' size='outline' className=''>
         Superiority
       </Button>
+      <AnimatedHeading className='text-display-xl mt-2 mb-10 text-center font-extrabold text-neutral-900'>
+        Built with design, speed, and UX in mind.
+      </AnimatedHeading>
+      <div className='mt-12 grid grid-cols-1 gap-6 md:grid-cols-2'>
+        <div className='bg-secondary-100 rounded-3xl p-6 text-black'>
+          <h1 className='text-display-xs text-center font-bold'>
+            Working With me
+          </h1>
+          <ul>
+            {withMe.map((data, index) => (
+              <React.Fragment key={index}>
+                <li className='my-2 flex items-center gap-2 text-lg font-bold'>
+                  <Image
+                    alt='star'
+                    src='/icons/superiority-star-me.svg'
+                    width={32}
+                    height={32}
+                  />
+                  {data.text}
+                </li>
+                <hr className='border-[#FDFDFD]/50' />
+              </React.Fragment>
+            ))}
+          </ul>
+        </div>
+        <div className='rounded-3xl border border-neutral-300 p-6 text-black'>
+          <h1 className='text-display-xs text-center font-bold'>
+            Working With other
+          </h1>
+          <ul>
+            {withOther.map((data, index) => (
+              <React.Fragment key={index}>
+                <li className='my-2 flex items-center gap-2 text-lg font-bold'>
+                  <Image
+                    alt='star'
+                    src='/icons/superiority-star-other.svg'
+                    width={32}
+                    height={32}
+                  />
+                  {data.text}
+                </li>
+                <hr className='border-neutral-300' />
+              </React.Fragment>
+            ))}
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
---
+++ src/constants/superiority-data.ts
@@ -0,0 +1,21 @@
+interface SuperiorityData {
+  text: string;
+}
+
+export const withMe: SuperiorityData[] = [
+  { text: 'React Expert' },
+  { text: 'Precise Website Implementation' },
+  { text: 'TypeScript Proficiency' },
+  { text: 'Clean, Maintainable Code' },
+  { text: 'Responsive Website Development' },
+  { text: 'UI Design Proficiency (Figma)t' },
+];
+
+export const withOther: SuperiorityData[] = [
+  { text: 'Basic React Knowledge' },
+  { text: 'Inconsistent Design Translation' },
+  { text: 'Little to No TypeScript Knowledge' },
+  { text: 'Unstructured Code' },
+  { text: 'Inconsistent Responsiveness' },
+  { text: 'No Design Skills' },
+];
