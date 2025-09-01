4913cae806f8f39225363548c28356fc9744900d
adi <adisudana@gmail.com>
2025-09-01 08:13:11 +0700

    fix partial page

--- src/app/home/partials/portfolio.tsx
+++ src/app/home/partials/portfolio.tsx
@@ -1,6 +1,15 @@
+import Image from 'next/image';
 import React from 'react';
 
+import AnimatedHeading from '@/components/ui/animated-heading';
 import { Button } from '@/components/ui/button';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from '@/components/ui/carousel';
 
 const Portfolio = () => {
   return (
@@ -9,6 +18,39 @@ const Portfolio = () => {
       <Button variant='outline' size='xs' className='mx-auto'>
         Portfolio
       </Button>
+      <AnimatedHeading className='text-display-xl mt-2 mb-10 text-center font-extrabold text-neutral-900'>
+        Built with Passion, Shipped with Precision
+      </AnimatedHeading>
+      <Carousel>
+        <CarouselContent className='-ml-1'>
+          <CarouselItem className='pl-1 md:basis-1/2 lg:basis-1/3'>
+            <Image
+              src='/images/portofolio1.png'
+              alt='portofolio1'
+              width={373}
+              height={373}
+            />
+          </CarouselItem>
+          <CarouselItem className='pl-1 md:basis-1/2 lg:basis-1/3'>
+            <Image
+              src='/images/portofolio2.png'
+              alt='portofolio1'
+              width={373}
+              height={373}
+            />
+          </CarouselItem>
+          <CarouselItem className='pl-1 md:basis-1/2 lg:basis-1/3'>
+            <Image
+              src='/images/portofolio3.png'
+              alt='portofolio1'
+              width={373}
+              height={373}
+            />
+          </CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
     </section>
   );
 };
